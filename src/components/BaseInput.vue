7f4fc4d2940425f19a85ee34c8d26d9f16cff242
linfengOu <linfeng.ou@gmail.com>
2021-09-21 16:41:49 +0800

    first commit

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+defineProps({
+    modelValue: String,
+})
+</script>
+
+<template>
+    <input
+        :value="modelValue"
+        @input="$emit('update:modelValue', (<HTMLInputElement>$event.target).value)"
+    />
+</template>
+
+<style scoped>
+</style>
