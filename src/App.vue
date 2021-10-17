8cbf25525f9e7ee2acb087db693def312ec5016e
linfengOu <linfeng.ou@gmail.com>
2021-10-17 20:30:43 +0800

    Refined user form

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,9 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
+}
+
+#app * {
+  box-sizing: border-box;
 }
 </style>
