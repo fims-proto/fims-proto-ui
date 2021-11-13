589c3c35ecb232643d505ef0a06ed9e4d30aa408
linfengOu <linfeng.ou@gmail.com>
2021-11-13 17:02:07 +0800

    Draft sob main and detailed page

--- src/components/SobDetail.vue
+++ src/components/SobDetail.vue
@@ -1,3 +1,4 @@
 <template>
   <p>sob detail page</p>
+  <router-link :to="{ name: 'voucherMain' }">show vouchers</router-link>
 </template>
--- src/components/SobMain.vue
+++ src/components/SobMain.vue
@@ -1,3 +1,27 @@
+<script lang="ts">
+import { defineComponent, onMounted, ref } from 'vue';
+import Sob from '../domain/Sob';
+
+export default defineComponent({
+  setup() {
+    const sobs = ref<Array<any>>()
+
+    onMounted(async () => {
+      sobs.value = await Sob.getAllSods()
+    })
+
+    return {
+      sobs
+    }
+  }
+})
+</script>
+
 <template>
-  <p>sob main page</p>
+  <div>
+    <p>sob main page</p>
+    <div v-for="sob in sobs" :key="sob.Id">
+      <router-link :to="{ name: 'sobDetail', params: { sobId: sob.Id } }">{{ sob.Name }}</router-link>
+    </div>
+  </div>
 </template>
