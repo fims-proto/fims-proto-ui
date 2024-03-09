e0799db1253a10167253b7af0a6ab545cc8e2f91
Linfeng Ou <linfeng.ou@gmail.com>
2024-03-09 16:50:42 +0800

    K8s readyness

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,30 @@
-import { defineConfig } from 'vite'
+import { defineConfig, type UserConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
 import { HeadlessUiResolver, VueUseComponentsResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    host: '127.0.0.1',
-    port: 5001,
-  },
-  plugins: [
-    vue(),
-    Components({
-      resolvers: [HeadlessUiResolver(), VueUseComponentsResolver(), HeroiconsResolver()],
-    }),
-  ],
+export default defineConfig(({ command }) => {
+  const config: UserConfig = {
+    plugins: [
+      vue(),
+      Components({
+        resolvers: [HeadlessUiResolver(), VueUseComponentsResolver(), HeroiconsResolver()],
+      }),
+    ],
+  }
+
+  if (command === 'serve') {
+    // dev
+    config.server = {
+      host: '127.0.0.1',
+      port: 5001,
+    }
+    return config
+  } else {
+    // build
+    return config
+  }
 })
 
 function HeroiconsResolver() {
