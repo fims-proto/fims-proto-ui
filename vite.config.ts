87bcc89b8bcdaf0e77067df2baa0aa4b43cbd21f
Linfeng Ou <linfeng.ou@gmail.com>
2024-03-31 15:29:01 +0800

    Nginx config for production

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { HeadlessUiResolver, VueUseComponentsResolver } from 'unplugin-vue-compo
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
   const config: UserConfig = {
+    envDir: './env',
     plugins: [
       vue(),
       Components({
